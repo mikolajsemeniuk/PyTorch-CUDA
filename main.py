c6cd4cb745ee38ae663402cd7591eb291bb345ed
mikolaj semeniuk <m.semeniuk@icloud.com>
2021-11-06 16:30:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ print(f'''
 	device_count: {torch.cuda.device_count()}
 	get_device_name: {torch.cuda.get_device_name(0)}
 ''')
-'''
-	cuda enable: True
-        current_device: 0
-        device: <torch.cuda.device object at 0x000001FC1984DCD0>
-        device_count: 1
-        get_device_name: NVIDIA GeForce GTX 1650 Ti
-'''
+#	cuda enable: True
+#       current_device: 0
+#       device: <torch.cuda.device object at 0x000001FC1984DCD0>
+#       device_count: 1
+#       get_device_name: NVIDIA GeForce GTX 1650 Ti
